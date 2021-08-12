f1f6955cca60f218a75c62582cd8c317bbbb0eac
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-11 21:36:16 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const {addUser, removeUser, getUser, getUsersInRoom} = require('./users');
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 const router = require('./routes');
 
 const app = express();
+
+app.use(cors())
 
 app.use(router);
 
